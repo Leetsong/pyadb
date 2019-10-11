81f5afa2e02fe93962c504090bee85a4bd844308
Leetsong <leetsong.lc@gmail.com>
2019-10-11 22:54:35 +0800

    Terminate proc when returned True

--- __init__.py
+++ __init__.py
@@ -46,98 +46,6 @@ def _from_proc_output(output: bytes) -> str:
     return str(output, encoding='utf-8').strip(' \t\n')
 
 
-def _async_raise(tid, exctype):
-    """
-    Raises an exception in the threads with id tid
-    :param tid: it of a thread
-    :param exctype: type of exception to be thrown
-    """
-    if not inspect.isclass(exctype):
-        raise TypeError("Only types can be raised (not instances)")
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid),
-                                                     ctypes.py_object(exctype))
-    if res == 0:
-        raise ValueError("invalid thread id")
-    elif res != 1:
-        # if it returns a number greater than one, you're in trouble,
-        # and you should call it again with exc=NULL to revert the effect
-        ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), None)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
-
-
-class InterruptableThread(Thread):
-
-    class _InterruptThread(Exception):
-        pass
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._interrupted = threading.Event()
-
-    def interrupted(self):
-        return self._interrupted.is_set()
-
-    def interrupt(self):
-        self._raise_exc(self._InterruptThread)
-
-    def run(self):
-        try:
-            super().run()
-        except self._InterruptThread:
-            self._interrupted.set()
-
-    def _get_my_tid(self):
-        """
-        Determines this (self's) thread id
-
-        CAREFUL : this function is executed in the context of the caller
-        thread, to get the identity of the thread represented by this
-        instance.
-        """
-        if not self.is_alive():
-            raise ThreadError("the thread is not active")
-
-        # do we have it cached?
-        if hasattr(self, "_thread_id"):
-            return self._thread_id
-
-        # no, look for it in the _active dict
-        for tid, tobj in threading._active.items():
-            if tobj is self:
-                self._thread_id = tid
-                return tid
-
-        # TODO: in python 2.6, there's a simpler way to do : self.ident
-
-        raise AssertionError("Could not determine the thread's id")
-
-    def _raise_exc(self, exctype):
-        """
-        Raises the given exception type in the context of this thread.
-
-        If the thread is busy in a system call (time.sleep(),
-        socket.accept(), ...), the exception is simply ignored.
-
-        If you are sure that your exception should terminate the thread,
-        one way to ensure that it works is:
-
-            t = ThreadWithExc( ... )
-            ...
-            t.raiseExc( SomeException )
-            while t.isAlive():
-                time.sleep( 0.1 )
-                t.raiseExc( SomeException )
-
-        If the exception is to be caught by the thread, you need a way to
-        check that your thread has caught it.
-
-        CAREFUL : this function is executed in the context of the
-        caller thread, to raise an excpetion in the context of the
-        thread represented by this instance.
-        """
-        _async_raise(self._get_my_tid(), exctype)
-
-
 class NonBlockingReader:
 
     class TimeoutException(Exception):
@@ -146,7 +54,7 @@ class NonBlockingReader:
     def __init__(self, stream):
         self._stream = stream
         self._queue = Queue()
-        self._thread = InterruptableThread(target=self._run)
+        self._thread = Thread(target=self._run)
         self._thread.start()
 
     def empty(self):
@@ -168,7 +76,6 @@ class NonBlockingReader:
 
     def close(self):
         if self._thread.is_alive():
-            self._thread.interrupt()
             self._thread.join()
 
     def _run(self):
@@ -663,6 +570,7 @@ class Adb:
                 binary_line = reader.readline(timeout / 1000)  # read one binary line
             except reader.TimeoutException:
                 if callback(True, ''):  # callback to give opportunity for termination
+                    proc.terminate()
                     break
                 continue
             if binary_line is None:  # done reading
@@ -674,7 +582,6 @@ class Adb:
                 err = _from_proc_output(t.read())
                 reader.close()
                 t.close()
-                proc.terminate()
                 raise CalledProcessError(returncode=rc, cmd=' '.join(final_adb_cmd),
                                          output=None, stderr=err)
             try:
@@ -683,11 +590,11 @@ class Adb:
                 pass  # ignored
             else:
                 if callback(False, text_line):
+                    proc.terminate()
                     break
 
         reader.close()
         t.close()
-        proc.terminate()
         self._reset()  # reset state after each command
 
     def _exec_command_to_file(self, adb_cmd, dest_file_handler):
@@ -721,34 +628,14 @@ class Adb:
 if __name__ == '__main__':
     adb = Adb(False, False)
 
-    def test_thread():
-        from time import sleep
-
-        def never_stop():
-            counter = 1
-            while True:
-                print(counter)
-                sleep(1)
-                counter += 1
-
-        thread = InterruptableThread(target=never_stop)
-        thread.start()
-
-        sleep(5)
-        thread.interrupt()
-
-        print(thread.interrupted())
-        thread.join()
-        print(thread.interrupted())
-
-        print('Exit')
-
     def test_logcat():
         from threading import Thread
 
         interrupted = False
 
         def on_logcat(timeout, line) -> bool:
+            if interrupted:
+                return True
             if timeout:
                 return False
             if line is None or line == '':
